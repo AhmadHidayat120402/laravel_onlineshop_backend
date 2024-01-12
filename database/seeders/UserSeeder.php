27c4a4632a8c0de030ec9c43fc82f99bd652ec04
Ahmad Hidayat <ahmadhidayat120402@gmail.com>
2024-01-12 09:34:12 +0700

    laravel online shop backend

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // general dibuatkan
+        User::factory(9)->create();
+
+        // buat statis
+        User::factory()->create([
+            'name' => 'Admin Dayat',
+            'email' => 'dayatfic12@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
